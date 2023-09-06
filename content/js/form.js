7b06c042683f5bf3837b1df04b2b30a6694f7531
senbagavalli04 <142738030+senbagavalli04@users.noreply.github.com>
2023-09-06 03:58:42 -1200

    Add files via upload

---
+++ content/js/form.js
@@ -0,0 +1,11 @@
+function validateForm() {
+    // Basic form validation
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+
+    if (name === "" || email === "" || message === "") {
+        alert("Please fill in all fields");
+        return false; // Prevent form submission
+    }
+}
